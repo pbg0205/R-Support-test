713fb03c874038408fc5a81909dd12eff26e3620
cooper <pbg0205@naver.com>
2021-10-19 17:13:01 +0900

    first commit

---
+++ rsupport-homework/src/main/java/kr/co/rsupport/cooper/rsupporthomework/RsupportHomeworkApplication.java
@@ -0,0 +1,13 @@
+package kr.co.rsupport.cooper.rsupporthomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsupportHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsupportHomeworkApplication.class, args);
+	}
+
+}
---
+++ rsupport-homework/src/test/java/kr/co/rsupport/cooper/rsupporthomework/RsupportHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.rsupport.cooper.rsupporthomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsupportHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
