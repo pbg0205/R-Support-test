ad2f4566ffde3f0620ac6ead423ea2a6460cb418
cooper <pbg0205@naver.com>
2021-10-21 22:06:35 +0900

    feat : NoticeRepository 생성
    - JpaRepository interface를 상속받은 NoticeRepository 생성

---
+++ rsupport-homework/src/main/java/kr/co/rsupport/cooper/rsupporthomework/notice/domain/NoticeRepository.java
@@ -0,0 +1,6 @@
+package kr.co.rsupport.cooper.rsupporthomework.notice.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+}
