e0ce20ceab8085a115876fe8990a7411e6e4ad72
cooper <pbg0205@naver.com>
2021-10-22 01:30:15 +0900

    feat : RedisNoticeRepository 생성

---
+++ rsupport-homework/src/main/java/kr/co/rsupport/cooper/rsupporthomework/notice/domain/RedisNoticeRepository.java
@@ -0,0 +1,6 @@
+package kr.co.rsupport.cooper.rsupporthomework.notice.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RedisNoticeRepository extends CrudRepository<RedisNotice, Long> {
+}
